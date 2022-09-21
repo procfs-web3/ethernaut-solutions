19654331fe59f60967f6ab7ac839e51506d61530
procfs <woosun.song@dream-academy.io>
2022-09-22 00:58:15 +0900

    complete coin flip

---
+++ coin-flip/fire.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cast send $@ "fire()" \
+--rpc-url https://flashy-ultra-dew.rinkeby.discover.quiknode.pro/8a52ef5715cebdc3e0aad57bb02766959471e863/ \
+--private-key 0x04e9e9c3b8cb0401030132e07a435dd3c7b73da0f9cc0b787a93f8e876da6a84
